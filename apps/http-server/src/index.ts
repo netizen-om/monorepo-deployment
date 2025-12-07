0c6922f43180b4c1c1cd1d62a1a3f8733daf5f93
netizen-om <borisagarom@gmail.com>
2025-12-07 12:08:02 +0530

    added basic logic for ws-server, http-server and nextjs app

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from "express";
+import { client } from "@repo/db/client";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
+});
+
+app.post("/signup", async (req: Request, res: Response) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    res.json({
+      Error: "username or passowrd not found!!!!!!!!!!!!!!",
+    });
+  }
+
+  const user = await client.user.create({
+    data: {
+      username,
+      password,
+    },
+  });
+
+  res.json({
+    message: "Signup Successfull",
+    id : user.id
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
-export const client = new PrismaClient();
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const client = new PrismaClient({
+  adapter,
+});
